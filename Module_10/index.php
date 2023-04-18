4c9404cce6b7dfb2677cef7b63a30ad784bc7476
Abu Yousuf Tamzid <abuyousuftamzid741@gmail.com>
2023-04-18 17:48:12 +0600

    Module 11

--- Module_10/index.php
+++ Module_10/index.php
@@ -1,16 +1,17 @@
 <?php
 include_once 'nav.php'
 ?>
+<section class="banner-wrapper">
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img class="d-block w-100 slide-img" src="/images/slide1.jpg" alt="First slide">
+            <img class="d-block slide-img w-100 " src="/images/slide1.jpg" alt="First slide">
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100 slide-img " src="/images/slide2.jpg" alt="Second slide">
+            <img class="d-block slide-img w-100  " src="/images/slide2.jpg" alt="Second slide">
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100 slide-img" src="/images/slide3.jpg" alt="Third slide">
+            <img class="d-block slide-img w-100 " src="/images/slide3.jpg" alt="Third slide">
         </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -22,6 +23,7 @@ include_once 'nav.php'
         <span class="sr-only">Next</span>
     </a>
 </div>
+</section>
 
 
 <!-- ABOUT US -->
