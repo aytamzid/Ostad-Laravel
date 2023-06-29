fc7d0301cfb7624a6a94399ce3365cafde447300
Abu Yousuf Tamzid <abuyousuftamzid741@gmail.com>
2023-06-30 01:27:56 +0600

    Query Update

--- Portfolio_Site_Mod18/Portfolio Project/routes/web.php
+++ Portfolio_Site_Mod18/Portfolio Project/routes/web.php
@@ -16,13 +16,13 @@ Route::get('/resume',[ResumeController::class,'page']);
 
 
 // Ajax Call Routes
-Route::get('/heroData',[HomeController::class,'heroData'])
-Route::get('/aboutData',[HomeController::class,'aboutData'])
-Route::get('/socialData',[HomeController::class,'socialData'])
-Route::get('/projectsData',[ProjectController::class,'projectsData'])
-Route::get('/resumeLink',[ResumeController::class,'resumeLink'])
-Route::get('/experiencesData',[ResumeController::class,'experiencesData'])
-Route::get('/educationData',[ResumeController::class,'educationData'])
-Route::get('/skillsData',[ResumeController::class,'skillsData'])
-Route::get('/languageData',[ResumeController::class,'languageData'])
-Route::post('/contactRequest',[ContactController::class,'contactRequest'])
+Route::get('/heroData',[HomeController::class,'heroData']);
+Route::get('/aboutData',[HomeController::class,'aboutData']);
+Route::get('/socialData',[HomeController::class,'socialData']);
+Route::get('/projectsData',[ProjectController::class,'projectsData']);
+Route::get('/resumeLink',[ResumeController::class,'resumeLink']);
+Route::get('/experiencesData',[ResumeController::class,'experiencesData']);
+Route::get('/educationData',[ResumeController::class,'educationData']);
+Route::get('/skillsData',[ResumeController::class,'skillsData']);
+Route::get('/languageData',[ResumeController::class,'languageData']);
+Route::post('/contactRequest',[ContactController::class,'contactRequest']);
