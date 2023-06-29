c6c515f7407958bd9f58ff12e59207a3eb8976ab
Abu Yousuf Tamzid <abuyousuftamzid741@gmail.com>
2023-06-30 00:39:39 +0600

    Controller Update

--- Portfolio_Site_Mod18/Portfolio Project/database/migrations/2023_06_29_180200_create_abouts_table.php
+++ Portfolio_Site_Mod18/Portfolio Project/database/migrations/2023_06_29_180200_create_abouts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('abouts', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('title',100);
             $table->text('details');
             $table->timestamp('created_at')->useCurrent();
