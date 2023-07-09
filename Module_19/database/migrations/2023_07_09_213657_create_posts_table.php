97f29a8421d515e0c3f94c9565077ef10775394e
Abu Yousuf Tamzid <abuyousuftamzid741@gmail.com>
2023-07-10 04:03:30 +0600

    update

---
+++ Module_19/database/migrations/2023_07_09_213657_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',200);
+            $table->string('author',200);
+            $table->text('content');
+            $table->string('thumbnail',200);
+            $table->string('hero_background',200);
+            $table->string('author_image',200);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
--- Module_19/routes/web.php
+++ Module_19/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Page View Routes
+Route::get('/', [HomeController::class, 'page']);
+Route::get('/article', [ArticleController::class, 'page']);
