219fb6353761d5e012a4f73328e3361985773a83
Abu Yousuf Tamzid <abuyousuftamzid741@gmail.com>
2023-08-31 23:10:38 +0600

    Update Mod 25

--- Module_25/routes/web.php
+++ Module_25/routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\LeaveController;
+use App\Http\Controllers\ManagerController;
+use App\Http\Middleware\TokenVerificationMiddleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +18,42 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('page.loginPage');
 });
+
+Route::controller(ManagerController::class)->group(function (){
+    Route::post('/restration-manager','managerRegistration');
+    Route::post('/login','Login');
+    Route::get('/logout','Logout');
+    Route::get('/manager_employe_list', 'employeeLeavesList')->middleware(TokenVerificationMiddleware::class);
+    Route::post('/manager_employe_listBy-id', 'employeeLeavesListById')->middleware(TokenVerificationMiddleware::class);
+  
+
+     //page route
+     Route::get('/managerDashboard','managerDashPage')->middleware(TokenVerificationMiddleware::class);
+     Route::get('/manager-reg-page','managerRegistrationPage');
+});
+
+Route::controller(EmployeeController::class)->group(function (){
+    Route::post('/restration-employee','employeeRegistration');
+    Route::get('/total-employee','TotalEmployee')->middleware(TokenVerificationMiddleware::class);
+
+
+    //page route
+    Route::get('/employeeDashboard','employeeDashPage')->middleware(TokenVerificationMiddleware::class);
+    Route::get('/emp-reg-page','EmpRegPage');
+});
+
+Route::controller(LeaveController::class)->group(function (){
+     
+    Route::get('/get-leave-list','GetList')->middleware(TokenVerificationMiddleware::class);
+    Route::post('/create-leave','CreateLeave')->middleware(TokenVerificationMiddleware::class);
+    Route::post('/updte-leave-status','updateStatus')->middleware(TokenVerificationMiddleware::class);
+
+    Route::get('/total-leaveApplication','TotalLeaveApplication')->middleware(TokenVerificationMiddleware::class);
+    Route::get('/total-pending-by-emp','TotalPendingbyEmloyee')->middleware(TokenVerificationMiddleware::class);
+    Route::get('/total-pending','TotalPending')->middleware(TokenVerificationMiddleware::class);
+});
+
+
+
